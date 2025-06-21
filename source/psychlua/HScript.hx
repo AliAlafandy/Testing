27a024ff96a8f21858fb2e5370fe8409354e6b94
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2025-06-21 16:26:20 +0300

    Update HScript.hx

--- source/psychlua/HScript.hx
+++ source/psychlua/HScript.hx
@@ -358,7 +358,7 @@ class HScript extends SScript
 		}
 	}
 
-	public function executeCode(?funcToRun:String = null, ?funcArgs:Array<Dynamic> = null):Tea {
+	public function executeCode(?funcToRun:String = null, ?funcArgs:Array<Dynamic> = null):TeaCall {
 		if (funcToRun == null) return null;
 
 		if(!exists(funcToRun)) {
@@ -390,7 +390,7 @@ class HScript extends SScript
 		return callValue;
 	}
 
-	public function executeFunction(funcToRun:String = null, funcArgs:Array<Dynamic>):Tea {
+	public function executeFunction(funcToRun:String = null, funcArgs:Array<Dynamic>):TeaCall {
 		if (funcToRun == null) return null;
 		return call(funcToRun, funcArgs);
 	}
